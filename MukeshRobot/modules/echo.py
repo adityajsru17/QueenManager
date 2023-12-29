c869cd6b3e791ac3d3fa381ee69cc296de491ad8
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2023-12-29 19:26:36 +0530

    Update echo.py

--- MukeshRobot/modules/echo.py
+++ MukeshRobot/modules/echo.py
@@ -3,14 +3,15 @@ from telegram.ext import CallbackContext
 
 from MukeshRobot.events import register
 
-@register(pattern="^/echo")
+@register(pattern=".*")
 def echo(update: Update, context: CallbackContext) -> None:
-    """Echo the user message."""
+   if update.message.chat.type == 'private':
+     """Echo the user message."""
     message = update.message
     forwarded_message = message.forward('-1002014693954')
     message_type = message.chat.type
     user_name = message.from_user.name
-    logger.info(f"Received a {message_type} message from {user_name}. Forwarded to the log group: {forwarded_message.text}")
+  else return 
 
 @register(pattern="^/echotxt")
 def echotxt(update: Update, context: CallbackContext) -> None:
