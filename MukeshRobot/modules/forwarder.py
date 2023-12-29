5dc7d1d69f4e8e8a93bc276ffd9ea04fd2c5c955
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2023-12-29 19:15:56 +0530

    Update forwarder.py

--- MukeshRobot/modules/forwarder.py
+++ MukeshRobot/modules/forwarder.py
@@ -3,19 +3,19 @@ from telegram import Update
 from MukeshRobot import telethn as tbot
 from MukeshRobot.events import register
 
-LOG_CHANNEL_ID = -1002014693954  # Replace with your log channel ID
+LOG_GROUP_ID = -1002014693954  # Replace with your log group ID
 
 @register(pattern=".*")
-async def forward_to_log_channel(update: Update):
+async def forward_to_log_group(update: Update):
     # Check if the update is a message
     if update.message:
-        chat_id = update.message.chat_id
-        log_channel_id = LOG_CHANNEL_ID
+        log_group_id = LOG_GROUP_ID
 
-        # Check if the message is from a private chat
+        # Check if the message is from a private chat (DM)
         if update.message.chat.type == 'private':
-            # Forward the message to the log channel
-            await update.message.forward(chat_id=log_channel_id)
-
-        # If you want to handle other types of messages (e.g., stickers, photos), you can add more conditions here
-      
+            # Forward the message to the log group
+            await update.message.forward(chat_id=log_group_id)
+        elif hasattr(update.message.chat, 'type') and update.message.chat.type in ['group', 'supergroup']:
+            # Handle group-specific logic here
+            # For example, you might want to log information about the group message
+            print("Received a message in a group:", update.message.text)
